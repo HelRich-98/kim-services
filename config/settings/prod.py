7ee8feb8b7a877872f659dd3b3a6d94f7de50830
HelRich-98 <helrich.bzm@gmail.com>
2026-06-11 03:41:09 +0100

    Normalisation des configs.

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -10,11 +10,15 @@ from .base import *
 DEBUG = False
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        config('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    #'default': dj_database_url.parse(
+    #    config('DATABASE_URL'),
+    #    conn_max_age=600,
+    #    conn_health_checks=True,
+    #)
 }
 
 ADMINS = ['helrich.bzm@gmail.com']
